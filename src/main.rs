182767bba69f824308e47a4433ef93abb9a7dfa2
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2025-04-04 05:46:56 +0800

    feat: scan string tables

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use start::{systypes::document::RawDocument, sysutils::find::find_collection::find_collection, utils::insert::insert_one};
+use start::{systypes::document::RawDocument, sysutils::find::{find_collection::find_collection, scan::scan}, utils::insert::insert_one};
 
 fn main() {
     let mut db = start::in_memory();
@@ -7,5 +7,8 @@ fn main() {
     insert_one(&mut db.ss, "students", "gebert".as_bytes().to_vec());
     insert_one(&mut db.ss, "students", "leon".as_bytes().to_vec());
     let students = find_collection(&mut db.ss, "students");
+    if let Some(table) = students {
+        scan(&mut db.ss, table);
+    }
     println!("students col: {:?}", students);
 }
--- src/sysutils/find/scan.rs
+++ src/sysutils/find/scan.rs
@@ -1,19 +1,28 @@
 use start_storage::StartStorage;
 
-use crate::systypes::document::RawDocument;
+use crate::systypes::{collection::Collection, document::RawDocument};
 
-pub fn find(ss: &mut StartStorage, offset: usize, contains: &[u8]) -> Option<RawDocument> {
-    let mut next_offset = offset;
+pub fn scan(ss: &mut StartStorage, offset: usize) -> Option<RawDocument> {
+    let raw_doc = RawDocument::parse(ss, offset);
+    println!("RawDoc: {:?}", raw_doc);
+    let collection = Collection::parse(&raw_doc.content);
+    println!("Col: {:?}", collection);
 
+    let mut next_offset = collection.next_document as usize;
+    
     while next_offset != 0 {
         let raw_doc = RawDocument::parse(ss, next_offset);
         println!("RawDoc: {:?}", raw_doc);
 
-        if raw_doc.content.starts_with(contains) {
-            return Some(raw_doc);
+        if let Ok(text) = std::str::from_utf8(&raw_doc.content) {
+            println!("{}. '{}'", next_offset, text);
         }
 
-        next_offset = raw_doc.next_document as usize; 
+        if next_offset == offset as usize {
+            next_offset = collection.next_document as usize; // Move to the next document
+        } else {
+            next_offset = raw_doc.next_document as usize; // Move to the next document
+        }
     }
 
     None
